e105d824f658448ef0dff97708b827337996c4d8
Sukhman Singh <sukhmansinghbhogal@gmail.com>
2025-05-22 01:08:01 +0000

    Refactor cat and beer songs to use helper functions for better readability

--- kata/Lesson1Straight.py
+++ kata/Lesson1Straight.py
@@ -4,15 +4,19 @@ from kata.Song import Song
 class Lesson1Straight(Song):
     def sing_cat_song(self):
         self.sing("We gave the cat to a little kid")
-        self.sing("But the cat came back")
-        self.sing("The very next day")
-        self.sing("Oh the cat came back")
-        self.sing("We thought he was a goner")
-        self.sing("But the cat came back, he just wouldn't go away")
+        def box1():
+            self.sing("But the cat came back")
+            self.sing("The very next day")
+            self.sing("Oh the cat came back")
+            self.sing("We thought he was a goner")
+            self.sing("But the cat came back, he just wouldn't go away")
+        box1()
+
         self.sing("We sent the cat out on a boat")
-        self.sing("But the cat came back")
-        self.sing("The very next day")
-        self.sing("Oh the cat came back")
-        self.sing("We thought he was a goner")
-        self.sing("But the cat came back, he just wouldn't go away")
-        
+        def box2():
+            self.sing("But the cat came back")
+            self.sing("The very next day")
+            self.sing("Oh the cat came back")
+            self.sing("We thought he was a goner")
+            self.sing("But the cat came back, he just wouldn't go away")
+        box2()
--- kata/Lesson2Variable.py
+++ kata/Lesson2Variable.py
@@ -3,11 +3,10 @@ from kata.Song import Song
 
 class Lesson2Variable(Song):
     def sing_beer_song(self):
-        self.sing("100 bottles of beer on the wall")
-        self.sing("100 bottles of beer")
-        self.sing("Take one down, pass it around")
-        self.sing("99 bottles of beer on the wall")
-        self.sing("99 bottles of beer on the wall")
-        self.sing("99 bottles of beer")
-        self.sing("Take one down, pass it around")
-        self.sing("98 bottles of beer on the wall")
+        def beer():
+            for i in range (100,98,-1):
+                self.sing(f"{i} bottles of beer on the wall")
+                self.sing(f"{i} bottles of beer")
+                self.sing("Take one down, pass it around")
+                self.sing(f"{i-1} bottles of beer on the wall")
+        beer()
